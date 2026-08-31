9d3845991c4f7451042788bca51b6b27b7eaf52b
saturn2888 <saturn2888@gmail.com>
2026-08-30 23:09:35 -0500

    feat(web): standardize search clear buttons

---
+++ web/src/components/ClearIcon.tsx
@@ -0,0 +1,16 @@
+export const ClearIcon = () => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    height="18"
+    viewBox="0 0 18 18"
+    width="18"
+  >
+    <path
+      d="m4.5 4.5 9 9m0-9-9 9"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth="2"
+    />
+  </svg>
+)
